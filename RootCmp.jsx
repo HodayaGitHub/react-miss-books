07710579e529ee5bfc82becb21659512b701f0ab
HodayaGitHub <hodaya1812@gmail.com>
2023-12-10 15:14:06 +0200

    Add book details cmp

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -10,7 +10,7 @@ export function App() {
             </header>
             <main className="container">
                 {/* <HomePage /> */}
-                {/* <BookIndex /> */}
+                <BookIndex />
             </main>
         </section>
     )
--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,31 +1,42 @@
 // • <BookIndex> – the app we will be building: a CRUDL for books 
+{/* <BookIndex> - renders the filter and the list */ }
+
 import { bookService } from "../services/book.service.js"
 const { useState, useEffect } = React
 
-export function CarIndex() {
+export function BookIndex() {
 
-    // const [books, setBooks] = useState(null)
+    const [books, setBooks] = useState(null)
     // const [selectedCarId, setSelectedCarId] = useState(null)
     // const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter())
 
-    // useEffect(() => {
-    //     loadBooks()
-    //     return () => {
-    //         alert('Bye Bye')
-    //     }
-    // }, [])
+    useEffect(() => {
+        loadBooks()
+        return () => {
+            alert('Unmounted')
+        }
+    }, [])
 
-    // function loadBooks() {
-    //     bookService.query(books)
-    //         .then(books => setBooks(books))
-    //         .catch(err => console.log('err:', err))
-    // }
+    function loadBooks() {
+        bookService.query('bookDB')
+            .then(books => setBooks(books))
+            .catch(err => console.log('err:', err))
+    }
 
-    return (
-        <div></div>
-    )
-
-
-
-
+    if (books) {
+        return (
+            <div>
+                {books.map((book) => (
+                    <div key={book.id} className="book-container">
+                        <h2>{book.title}</h2>
+                        <p><strong>Authors:</strong> {book.authors.join(', ')}</p>
+                        <p><strong>Description:</strong> {book.description}</p>
+                        <img src={book.thumbnail} alt={`Thumbnail for ${book.title}`} />
+                    </div>
+                ))}
+            </div>
+        )
+    } else {
+        return <p>Loading books...</p>
+    }
 }
